7e82e68f8265fd20d6a72e662e0362a9e1fac99c
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-25 01:43:03 +0600

    post api added

--- index.js
+++ index.js
@@ -2,16 +2,53 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // connection step with mongodb
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET_PASSWORD}@cluster0.vrsqm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('electronic_tools_manufacturing').collection('products');
+        const bookingCollection = client.db('electronic_tools_manufacturing').collection('booking');
 
+        // all data load 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // load single data 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result)
+        })
+        // data create
+        app.post('/booking', async (req, res) => {
+            const doc = req.body;
+            const email = req.body;
+            const query = { email: email }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, email: exists })
+            }
+            const result = await bookingCollection.insertOne(doc)
+            return res.send({ success: true, result });
+        })
+    }
+    finally {
 
+    }
+
+}
+run().catch(console.dir);
 
 
 

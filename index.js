5c8c23fa37751a1ec136299ed1215534ae71460c
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-26 15:06:44 +0600

    get and post api added

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
         const userCollection = client.db('electronic_tools_manufacturing').collection('users');
         const reviewCollection = client.db('electronic_tools_manufacturing').collection('reviews');
         const profileCollection = client.db('electronic_tools_manufacturing').collection('profiles');
+        const addProductCollection = client.db('electronic_tools_manufacturing').collection('singleProduct');
 
         // all data load 
         app.get('/products', async (req, res) => {
@@ -61,6 +62,19 @@ async function run() {
             const result = await bookingCollection.insertOne(doc)
             return res.send({ success: true, result });
         })
+        // single product data post api
+        app.post('/singleProduct', async (req, res) => {
+            const doc = req.body;
+            const result = await addProductCollection.insertOne(doc);
+            res.send(result)
+        })
+        // data load single
+        app.get('/single', async (req, res) => {
+            const result = await addProductCollection.find().toArray();
+
+            res.send(result);
+        })
+
         // booking data load 
         app.get('/booking', verifyJWT, async (req, res) => {
             const email = req.query.email;
@@ -80,18 +94,33 @@ async function run() {
             const users = await userCollection.find().toArray();
             res.send(users);
         })
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
         // admin data
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: {
-                    role: 'admin'
-                },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc)
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: {
+                        role: 'admin'
+                    },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc)
 
-            res.send(result);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
         })
 
         // user data collection api or add or update
